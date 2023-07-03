3547c0ae34ecf367c00adb375a0e04c0020d583e
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-07-03 18:04:38 +0800

    assigning the objects and their properties

--- Fan Class/test_fan.py
+++ Fan Class/test_fan.py
@@ -2,4 +2,11 @@ from fan_class import Fan
 
 class TestFan:
 
+
     def testing_fan(self):
+
+        first_object = Fan("FAST", 10, "yellow",True )
+        second_object = Fan("MEDIUM", 5, "blue",False )
+
+test_fan = TestFan()
+test_fan.testing_fan()
