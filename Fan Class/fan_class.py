9da59d0318eacb928cdb31484ab06eca18f5fb8a
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-07-03 16:51:03 +0800

    created constructor with the private data fields

--- Fan Class/fan_class.py
+++ Fan Class/fan_class.py
@@ -10,14 +10,19 @@ class Fan:
     MEDIUM = 2
     FAST = 3
 
+# A constructor that creates a fan 
+    def __init__(self, speed, radius, color, on):
+
 # Private int data named "speed"
+        self.__speed = speed
 
 # Private bol data named on that specifies whether the fan is on (the default is False)
-
+        self.__on = on
 # Private float data named "radius"
+        self.__radius = radius
 
 # Private string data named "color"
+        self.__color = color
 
 # The getters and setters methods for all four data fields
 
-# A constructor that creates a fan 
