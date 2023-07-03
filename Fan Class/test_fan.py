d28fd10a290272175af794eced9a8cc1cbbd9cc0
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-07-03 18:28:50 +0800

    printing the properties of first and second object

--- Fan Class/test_fan.py
+++ Fan Class/test_fan.py
@@ -5,8 +5,11 @@ class TestFan:
 
     def testing_fan(self):
 
-        first_object = Fan("FAST", 10, "yellow",True )
-        second_object = Fan("MEDIUM", 5, "blue",False )
+        first_object = Fan("FAST", 10, "yellow", True )
+        second_object = Fan("MEDIUM", 5, "blue", False )
+
+        print(f"The First Fan's speed is {first_object.get_speed()} , radius is {first_object.get_radius()}, color is {first_object.get_color()}, and it is {first_object.on()}")
+        print(f"The Second Fan's speed is {second_object.get_speed()} , radius is {second_object.get_radius()}, color is {second_object.get_color()}, and it is {second_object.on()}")
 
 test_fan = TestFan()
 test_fan.testing_fan()
