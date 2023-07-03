a1ad416dfc9f9469582bad70e7f796772d65d7c6
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-07-03 16:32:50 +0800

    pseudocode take two ( i wasn't able to save the first )

--- Fan Class/fan_class.py
+++ Fan Class/fan_class.py
@@ -1,1 +1,17 @@
 #Dahan, Regine Fae M.    Class Fan    BSCPE 1-5
+
+# The Fan class
+
+# Three constants ( SLOW = 1, MEDIUM =2, FAST =3)
+
+# Private int data named "speed"
+
+# Private bol data named on that specifies whether the fan is on (the default is False)
+
+# Private float data named "radius"
+
+# Private string data named "color"
+
+# The getters and setters methods for all four data fields
+
+# A constructor that creates a fan 
