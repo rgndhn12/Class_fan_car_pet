ecfe2df5146f3675a3db17111a22c5ec011e240c
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-07-04 16:10:24 +0800

    added features: pyfiglet,time,colorama

--- Fan Class/test_fan.py
+++ Fan Class/test_fan.py
@@ -1,4 +1,21 @@
+import pyfiglet
+import time
+import colorama
+from colorama import Fore
+colorama.init()
+
 from fan_class import Fan
+
+border = "✪" * 90
+border_2 = " ☪ " * 30
+print(border_2)
+print(border)
+print(Fore.BLUE+pyfiglet.figlet_format("FAN CLASS",font="arrows"))
+print(Fore.LIGHTWHITE_EX+border)
+print(border_2)
+time.sleep(2)
+print(Fore.LIGHTBLUE_EX+("The SPEED, RADIUS, COLOR, and SWITCH of the fans is loading..."))
+time.sleep(5)
 
 class TestFan:
 
@@ -8,8 +25,12 @@ class TestFan:
         first_object = Fan("FAST", 10, "yellow", True )
         second_object = Fan("MEDIUM", 5, "blue", False )
 
-        print(f"The First Fan's speed is {first_object.get_speed()} , radius is {first_object.get_radius()}, color is {first_object.get_color()}, and is the switch on? {first_object.on()}")
-        print(f"The Second Fan's speed is {second_object.get_speed()} , radius is {second_object.get_radius()}, color is {second_object.get_color()}, and is the switch on? {second_object.on()}")
+        print(border)
+        print(Fore.GREEN+(f"The First Fan's speed is {first_object.get_speed()} , radius is {first_object.get_radius()}, color is {first_object.get_color()}, and is the switch on? {first_object.on()}"))
+        print(border)
+        time.sleep(5)
+        print(Fore.LIGHTCYAN_EX+(f"The Second Fan's speed is {second_object.get_speed()} , radius is {second_object.get_radius()}, color is {second_object.get_color()}, and is the switch on? {second_object.on()}"))
+        print(border)
 
 test_fan = TestFan()
 test_fan.testing_fan()
