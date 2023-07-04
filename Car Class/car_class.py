b4bae178b5d56a1c5d32b59b51036d5568acbbeb
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-07-04 13:14:03 +0800

    pseudocode

--- Car Class/car_class.py
+++ Car Class/car_class.py
@@ -1,1 +1,22 @@
 #Dahan, Regine Fae M. Dahan   Car class BSCPE 1-5
+
+# The Car Class
+
+    # Car's year model
+    # Cars maker
+    # Car's current speed
+
+# __init__ method
+
+#methods
+    #accelerate
+    #brake
+    #get_speed
+
+#call accelerate method five times
+    #get the current speed
+    #display
+
+#call brake method five times
+    #get the current speed
+    #display
