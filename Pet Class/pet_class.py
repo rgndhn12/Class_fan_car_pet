f3be73dae7802268ccff3eaf43c382f29cc1082c
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-07-04 14:14:43 +0800

    pseudocode

--- Pet Class/pet_class.py
+++ Pet Class/pet_class.py
@@ -1,1 +1,20 @@
 #Dahan, Regine Fae M.   Pet Class  BSCPE 1-5
+
+# The Pet Class
+    #Pet's name
+    #Pet's type
+    #Pet's age
+
+#__init__method
+
+#methods
+    #set_name
+    #set_animal_type
+    #set_age
+    #get_name
+    #get_animal_type
+    #get_age
+
+#object of the class
+    #user's input(name,type,age of the pet)
+    #display
