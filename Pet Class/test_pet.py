bba6943a82e08d0f29fa8c23f89acd814f47f5fb
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-07-04 15:02:40 +0800

    displaying the pet's name, type and age

--- Pet Class/test_pet.py
+++ Pet Class/test_pet.py
@@ -7,6 +7,9 @@ pet_animal_type = input(" What's your pet's animal type? ")
 pet_age = input (" What's the age of your pet? ") 
 
 #object of the pet
-
-    
+pet_object = Pet(pet_name, pet_animal_type, pet_age)
+ 
 #display
+print(pet_object.get_name())
+print(pet_object.get_animal_type())
+print(pet_object.get_age())
